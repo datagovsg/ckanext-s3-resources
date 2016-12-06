7d60a6586cdf1752eae524225dc2ae04f69ccfa6
Joel Choo <joelchoo@gmail.com>
2016-12-06 15:04:36 +0800

    Upload zipfiles done in before_create/update. Added ability to archive resources

--- ckanext/datagovsg_s3_resources/plugin.py
+++ ckanext/datagovsg_s3_resources/plugin.py
@@ -2,6 +2,7 @@ import ckan.plugins as plugins
 import ckan.plugins.toolkit as toolkit
 import upload
 from routes.mapper import SubMapper
+import datetime
 
 class Datagovsg_S3_ResourcesPlugin(plugins.SingletonPlugin):
     plugins.implements(plugins.IResourceController, inherit=True)
@@ -26,24 +27,22 @@ class Datagovsg_S3_ResourcesPlugin(plugins.SingletonPlugin):
     # IResourceController
 
     def before_create(self, context, resource):
+        # Set timestamp for archiving
+        utc_datetime_now = datetime.datetime.utcnow().strftime("-%Y-%m-%dT%H:%M:%SZ")
+        context['s3_upload_timestamp'] = utc_datetime_now
         # Uploads resource to S3
         # WARNING: destructively modifies resource
         upload.upload_resource_to_s3(context, resource)
-
-
-    def after_create(self, context, resource):
         # Uploads package and resource zip files to S3
         upload.upload_zipfiles_to_s3(context, resource)
 
 
     def before_update(self, context, current, resource):
+        # Set timestamp for archiving 
+        utc_datetime_now = datetime.datetime.utcnow().strftime("-%Y-%m-%dT%H:%M:%SZ")
+        context['s3_upload_timestamp'] = utc_datetime_now
         # Uploads resource to S3
         # WARNING: destructively modifies resource
         upload.upload_resource_to_s3(context, resource)
-
-
-    def after_update(self, context, resource):
-        # Check if we need to upload the files to S3\
+        # Uploads package and resource zip files to S3
         upload.upload_zipfiles_to_s3(context, resource)
-
-
